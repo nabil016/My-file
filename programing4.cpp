694c333c6e5dfab1860814fedce4842127bcbd4c
nabil016 <153996513+nabil016@users.noreply.github.com>
2023-12-24 16:25:38 +0600

    Add files via upload

---
+++ programing4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main ()
+{
+   int num1,num2,num3,sum;
+   cout <<"enter 3 numbers : ";
+   cin>>num1 >>num2 >>num3;
+
+   sum = num1+num2+num3;
+
+   cout <<sum;
+
+    getch ();
+}
