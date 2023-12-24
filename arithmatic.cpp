6c1ef37030dd14414b804f754808957c6a0a3c0d
nabil016 <153996513+nabil016@users.noreply.github.com>
2023-12-24 13:45:55 +0600

    Add files via upload

---
+++ arithmatic.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+
+    float num1,num2;
+    cout<< "enter 2 number : ";
+    cin>>num1>>num2;
+
+    cout<<showpoint;
+
+    float sum = num1+num2;
+    cout <<"sum is : "<<sum;
+    cout <<endl;
+
+    float sub = num1-num2;
+    cout <<"subtraction is : "<<sub;
+    cout <<endl;
+
+    float mul = num1*num2;
+    cout <<"multiplaction is : "<<mul;
+    cout <<endl;
+
+    double div = (float) num1/num2;
+    cout <<"division is : "<<div;
+    cout <<endl;
+
+    getch();
+}
