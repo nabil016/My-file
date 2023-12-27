8528cfdfa5a84af133d5eee76cf34efa53ffee31
nabil016 <153996513+nabil016@users.noreply.github.com>
2023-12-27 08:38:38 +0600

    area of rectangle

---
+++ rectangle.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    double width,hight,area;
+
+    cout<<"enter width : ";
+    cin>> width;
+
+    cout<<"enter hight : ";
+    cin>> hight;
+
+    area = (double) width*hight;
+
+    cout<<"area of ractangle ="<<area;
+
+
+
+    getch();
+}
