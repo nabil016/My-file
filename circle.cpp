3a618e9b3cb284820253b5f3bd13991a5fcdf986
nabil016 <153996513+nabil016@users.noreply.github.com>
2023-12-27 08:42:03 +0600

    area of circle

---
+++ circle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    double radius,area;
+
+    cout<<"enter a radius : ";
+    cin>> radius;
+
+    area = 3.1416*radius*radius;
+
+    cout<<"area of radius ="<<area;
+
+
+
+    getch();
+}
